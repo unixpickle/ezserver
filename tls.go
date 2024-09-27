8409a977cefcfac0f177509816af7a5de69faf97
Alex Nichol <unixpickle@gmail.com>
2024-09-27 17:19:10 -0400

    getcert

--- tls.go
+++ tls.go
@@ -6,6 +6,7 @@ import (
 
 	"golang.org/x/crypto/acme"
 	"golang.org/x/crypto/acme/autocert"
+	"golang.org/x/net/idna"
 )
 
 // KeyCert represents a TLS key/centificate pair.
@@ -86,7 +87,14 @@ func (c *TLSConfig) ToConfig() (*tls.Config, *autocert.Manager, error) {
 		if c.ACMECacheDir != "" {
 			manager.Cache = autocert.DirCache(c.ACMECacheDir)
 		}
-		res.GetCertificate = manager.GetCertificate
+		res.GetCertificate = func(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+			if name, err := idna.Lookup.ToASCII(hello.ServerName); err == nil {
+				if result, ok := res.NameToCertificate[name]; ok {
+					return result, nil
+				}
+			}
+			return manager.GetCertificate(hello)
+		}
 	}
 
 	return res, manager, nil
